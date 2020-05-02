cd474daab8869c0aa8a1550862f094c561f10895
Wicksell Metellus <metellusux@gmail.com>
2020-05-02 14:09:26 -0700

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Wicksell Metellus on 5/2/20.
+//  Copyright © 2020 Metellus 3D. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
